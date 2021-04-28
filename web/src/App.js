e6f40d31ad9c702c686b9fd120d7c39fb067744b
Jungin <jungin3486@gmail.com>
2021-04-28 23:05:30 +0900

    add ButtonSearch.js MapContainer2.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-// import MapContiner from "./components/MapContiner";
 import SearchPlace from "./components/SearchPlace";
+import ButtonSearch from "./components/ButtonSearch";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={SearchPlace} />
+          <Route exact path="/ButtonSearch" component={ButtonSearch} />ß
         </Switch>
       </BrowserRouter>
     </>
---
+++ web/src/components/ButtonSearch.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import MapContainer2 from "./MapContainer2";
+
+const SearchPlace2 = () => {
+  const [inputText, setInputText] = useState("");
+  const [place, setPlace] = useState("");
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPlace(inputText);
+    setInputText("");
+  };
+
+  return (
+    <>
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input
+          placeholder="Search Place..."
+          onChange={onChange}
+          value={inputText}
+        />
+        <button type="submit">검색</button>
+      </form>
+      <MapContainer2 searchPlace={place} />
+    </>
+  );
+};
+
+export default SearchPlace2;
